95af2ce52cb18a26a6442bd5f7f0e2c14d531acb
26KMU01-KevinMorataya <moratayakevin16db@gmail.com>
2026-04-12 10:54:47 -0600

    Add files via upload

---
+++ NotasDeCursoC#.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Calculadora Basica");
+        string opc;
+        Console.WriteLine("A - Excelente.");
+        Console.WriteLine("B - Bueno");
+        Console.WriteLine("C - Regular");
+        Console.WriteLine("D - Deficiente");
+        Console.WriteLine("F - Reprobado");
+        Console.WriteLine("Digite la Opcion");
+        opc = Console.ReadLine();
+
+
+        switch (opc)
+        {
+            case "A":
+                Console.WriteLine("Tubo una calificación Excelente");
+                break;
+            case "B":
+                Console.WriteLine("Tubo una calificaion Bueno");
+                break;
+            case "C":
+                Console.WriteLine("Tubo una calificacion Regaluar");
+                break;
+            case "D":
+                Console.WriteLine("Tubo una calificacion Deficiente");
+                break;
+            case "F":
+                Console.WriteLine("Tubo una calificacion Reprobado");
+                break;
+            default:
+                Console.WriteLine("Digite un numero valido");
+                break;
+        }
+    }
+}
