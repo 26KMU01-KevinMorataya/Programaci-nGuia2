e95c0070fe99b75da14126bf1ce682482c0a9e75
26KMU01-KevinMorataya <moratayakevin16db@gmail.com>
2026-04-12 11:20:49 -0600

    Add files via upload

---
+++ Menu.psc
@@ -0,0 +1,31 @@
+Algoritmo Menu
+	Definir opc Como Entero
+	
+	Escribir "************************"
+	Escribir "*   POLLOS HERMANOS   *"
+	Escribir "************************"
+	
+	Escribir "Este es nuestro menú de la semana"
+	
+	Escribir "1 = Hambuerguesa."
+	Escribir "2 = Pizza."
+	Escribir "3 = Pollo Frito."
+	Escribir "4 = Ensalada."
+	
+	
+	Escribir Sin Saltar "Digite la opcion [] :"
+	Leer opc
+	
+	Segun opc Hacer
+		1:
+			Escribir "Hamburguesa En Camino"
+		2:
+			Escribir "Pizza En Camino"
+		3:
+			Escribir "Pollo Frito En Camino"
+		4:
+			Escribir "Ensalada En Camino"		
+		De Otro Modo:
+			Escribir "No tenemos esa comida en servicio"
+	FinSegun
+FinAlgoritmo
---
+++ MenuC#.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Pollos Hermanos");
+        int opc;
+        Console.WriteLine("Bienvenido al menu de Pollos Hermanos");
+        Console.WriteLine("1 - Hambuerguesa.");
+        Console.WriteLine("2 - Pizza");
+        Console.WriteLine("3 - Pollo Frito");
+        Console.WriteLine("4 - Ensalada");
+        Console.WriteLine("Digite la Opcion");
+        opc = Convert.ToInt32(Console.ReadLine());
+
+        switch (opc)
+        {
+            case 1:
+                Console.WriteLine("Su hambuerguesa esta en Camino .....");
+                break;
+            case 2:
+                Console.WriteLine("Su pizza esta en camino .....");
+                break;
+            case 3:
+                Console.WriteLine("Su pollo frito esta en camino ....");
+                break;
+            case 4:
+                Console.WriteLine("La ensalada esta en camino....");
+                break;
+            default:
+                Console.WriteLine("Esta comida no esta en servicio por el momento");
+                break;
+        }
+        Console.WriteLine("Gracias por su compra, vuelva pronto");  
+    }
+}
